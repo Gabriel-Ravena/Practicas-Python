46b7a5599b27f6e62c840a0e1985a008ef4bd546
Gabriel <gravena@nahualit.com>
2020-07-01 15:15:06 -0300

    herencia multiple

---
+++ figuras/figuras_geometricas.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+
+class FiguraGeometrica(ABC):
+
+    def __init__(self, alto, ancho):
+        self.alto = alto
+        self.ancho = ancho
+
+    @abstractmethod
+    def area(self):
+        pass
---
+++ herencia_multiple/prueba_cuadrado.py
@@ -0,0 +1,5 @@
+from herencia_multiple.cuadrado import Cuadrado
+
+cuadrado = Cuadrado(3, "rojo")
+print("Area del cuadrado: ", cuadrado.area())
+print("Color del cuadrado: ", cuadrado.color)
